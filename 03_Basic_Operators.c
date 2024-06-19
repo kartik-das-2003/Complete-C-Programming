cf1513361a9c81ce6a94dedb5ff6812d458f91d3
Kartik Das <142588930+kartik-das-2003@users.noreply.github.com>
2024-06-19 12:29:53 +0530

    Committed

--- 03_Basic_Operators.c
+++ 03_Basic_Operators.c
@@ -1,16 +1,36 @@
+//Concept Of --> Arithmetic,Comma,Assignment & Increment And Decrement Operator
+
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(void){
-	int num1,num2;
+	printf("\n\tArithmetic Operations:\n");
+	int num1,num2;//Comma Operator
+
 	printf("Input Number_1:");
 	scanf("%d",&num1);
 	printf("Input Number_2:");
 	scanf("%d",&num2);
+
 	printf("ADD VALUE:%d\n",(num1+num2));
 	printf("MULTIPLY VALUE:%d\n",(num1*num2));
 	printf("SUBTRACT VALUE:%d\n",(num1-num2));
 	printf("DIVISOR VALUE:%d\n",(num1/num2));
 	printf("MODULUS VALUE:%d\n",(num1%num2));
+
+	printf("\n\tIncrement & Decrement Operations:\n");
+	int x;
+	printf("Input X:");
+	scanf("%d",&x);
+	int a=x; //Assignment Opereator
+	printf("X++: %d\n",x++); //Increment After Print
+	printf("++X: %d\n",++a); //Increment Before Print
+
+	int y;
+	printf("\nInput Y:");
+	scanf("%d",&y);
+	int b = y;
+	printf("Y--: %d\n",y--); //Decrement After Print
+	printf("--Y: %d\n",--b); //Decrement Before Print
 	return 0;
 }
