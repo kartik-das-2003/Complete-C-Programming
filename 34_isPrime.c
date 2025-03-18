2743b88757d03a8f804d24cc8be16e43d2d8daeb
Kartik Das <142588930+kartik-das-2003@users.noreply.github.com>
2025-03-18 15:22:39 +0530

    Update 34_isPrime.c

--- 34_isPrime.c
+++ 34_isPrime.c
@@ -1,6 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+//Most Expected O(sqrt(n)) ---->>>Best
+#include <stdio.h>
+
+int main() {
+    int num, i;
+    int isPrime = 1;  // Assume the number is prime
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0;  // Numbers less than or equal to 1 are not prime
+    } else {
+        for (i = 2; i * i <= num; i++) {  // Check up to the square root of the number
+            if (num % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
+
+
+//Neither be late but easiest one ----> Apparantly best but not good O(n)
+/*
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPrime(int n) {
+    if (n <= 1)
+        return false;
+
+    // Check divisibility from 2 to n-1
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+
+    return true;
+}
+
+int main() {
+    int n = 11;
+    if( isPrime(n)) printf("%s","true");
+  	else printf("%s","false");
+    return 0;
+}
+*/
+
+//Good But More Complex but O(sqrt(n)) time
+/*
 int isPrime(int num) {
     if (num<=1) {
         return -1; // Not prime
@@ -31,3 +88,4 @@ int main(void){
     }
     return 0;
 }
+*/
